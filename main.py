5644c08a3106cc7ecf1b14399f5668c5c38512d7
trolle6 <44058284+trolle6@users.noreply.github.com>
2023-04-07 21:11:58 +0200

    edit gitignore

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ import discord
 from discord.ext import commands
 import boto3
 import asyncio
+import configparser
 
-# Discord bot settings
-TOKEN = 'Discord-Bot-Token'
-GUILD = 'Server-Name'
+# Read variables from configuration file
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-# AWS settings
-AWS_ACCESS_KEY_ID = 'AWS-Access-Key'
-AWS_SECRET_ACCESS_KEY = 'AWS-Secret-Key'
-AWS_REGION = 'Region-Code'
+TOKEN = config['TOKEN']
+GUILD = config['GUILD']
+AWS_ACCESS_KEY_ID = config['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = config['AWS_SECRET_ACCESS_KEY']
+AWS_REGION = config['AWS_REGION']
+whitelist = config['WHITELIST']
+whitelist_role_name = config['WHITELIST_ROLE_NAME']
+channel_name = config['CHANNEL_NAME']
 
 # Boto3 Polly client
 polly = boto3.client('polly', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_REGION)
@@ -24,10 +29,7 @@ bot = commands.Bot(command_prefix='', intents=intents)
 # Message queue settings
 message_queue = asyncio.Queue()
 is_speaking = False  # flag to check if bot is currently speaking
-
-# Whitelist settings
-whitelist = ['User1#0001', 'User2#0002', 'User3#0003']
-whitelist_role_name = 'Role-Name' # the name of the role that can add to the whitelist
+delay_seconds = float(config['MESSAGE_QUEUE_SETTINGS']['DELAY_SECONDS'])
 
 # Discord bot event handlers
 async def speak_message(vc):
@@ -40,7 +42,7 @@ async def speak_message(vc):
         file.write(response['AudioStream'].read())
         file.close()
         vc.play(discord.FFmpegPCMAudio('speech.mp3'))
-        await asyncio.sleep(0.5)  # add a short delay to ensure the file is created before playing
+        await asyncio.sleep(delay_seconds)  # add a short delay to ensure the file is created before playing
         while vc.is_playing():  # wait until the message finishes playing
             await asyncio.sleep(0.1)
         is_speaking = False  # set flag to False when bot is done speaking
@@ -65,27 +67,10 @@ async def add_whitelist(ctx, user):
 @bot.event
 async def on_message(message):
     global is_speaking
-    if message.channel.name == 'Channel-Name':
+    if message.channel.name == channel_name:
         if not message.author.bot:
             if message.author.name + '#' + message.author.discriminator in whitelist:
                 if message.content.strip():
                     if not is_speaking:  # check if bot is currently speaking
                         await message_queue.put(message.content)
-                        if not bot.voice_clients:
-                            vc = await message.author.voice.channel.connect()
-                            asyncio.ensure_future(speak_message(vc))
-                    else:
-                        await message.channel.send('Sorry, I am currently speaking. Please wait for me to finish before sending another message.')
-            else:
-                await message.channel.send('Sorry, you do not have access to use this bot.')
-    await bot.process_commands(message)
-
-# Asynchronous tasks
-async def check_voice_channels():
-    while True:
-        for vc in bot.voice_clients:
-            if not vc.is_playing() and not vc.is_paused() and not vc.is_connected():
-                await vc.disconnect()
-        await asyncio.sleep(4)
-
-async def check_empty_vc():
+                        if
